f1a5acc9c19e1923bca28175b9cc03b137195a9f
Jose1060 <josean-1060@hotmail.com>
2023-03-31 16:07:09 -0500

    Actualizacion

--- models/Restaurantes.js
+++ models/Restaurantes.js
@@ -1,24 +1,20 @@
-const {Schema,model} = require('mongoose')
-require('dotenv').config;
-
-
-
-
+const { Schema, model } = require("mongoose");
+require("dotenv").config;
 
 const restSchema = new Schema({
-    nombre: {type: String, require:false},
-    direccion: {type: String, require:false},
-    telefono: {type: String, require:false}, 
-    email: {type: String, require:false},
-    imagen: {type: String, require:false},
-    descripcion: {type: String, require:false},
-    latitud: {type: Number, require:false},
-    longitud: {type: Number,require:false},
-    etiquetas:[{type:String,require:false}],
-    N_calificaciones:{type:Number,require:false},
-    promedio:{type:Number,require:false},
-    total_puntos:{type:Number,require:false},
-    comentarios: [
+	nombre: { type: String, require: false },
+	direccion: { type: String, require: false },
+	telefono: { type: String, require: false },
+	email: { type: String, require: false },
+	imagen: { type: String, require: false },
+	descripcion: { type: String, require: false },
+	latitud: { type: Number, require: false },
+	longitud: { type: Number, require: false },
+	etiquetas: [{ type: String, require: false }],
+	N_calificaciones: { type: Number, require: false },
+	promedio: { type: Number, require: false },
+	total_puntos: { type: Number, require: false },
+	comentarios: [
 		{
 			idUsuario: { type: String, required: true },
 			contenido: { type: String, required: true },
@@ -26,24 +22,22 @@ const restSchema = new Schema({
 		},
 	],
 
-
-  carta: [
+	carta: [
 		{
 			nombre: { type: String, required: true },
 			detalle: { type: String, required: true },
 			imagen: { type: String, required: true },
-      precio: { type: Number, required: true },
-      ranking: { type: Number, required: true },
-    },
-	],
-
-    calificaciones: [
-		{
-			idUsuario: { type: String, required: true},
-            calificacion:{type:Number,require:false},
-			idRestaurante: { type: String, required: true },
+			precio: { type: Number, required: true },
+			ranking: { type: Number, required: true },
 		},
 	],
 
+	calificaciones: [
+		{
+			idUsuario: { type: String, required: true },
+			calificacion: { type: Number, require: false },
+			idRestaurante: { type: String, required: true },
+		},
+	],
 });
 module.exports = model("Restaurante", restSchema);
